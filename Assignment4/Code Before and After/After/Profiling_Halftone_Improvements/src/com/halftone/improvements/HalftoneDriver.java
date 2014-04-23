2022cfa8a0ab3e1d5670acd3d85e7e1a770a8200
Chantel Garcia <chantel.garcia@gmail.com>
2014-04-23 18:40:04 +1000

    Updated Profiling Report, Driver for "After" code and snapshots for before and after

--- Assignment4/Code Before and After/After/Profiling_Halftone_Improvements/src/com/halftone/improvements/HalftoneDriver.java
+++ Assignment4/Code Before and After/After/Profiling_Halftone_Improvements/src/com/halftone/improvements/HalftoneDriver.java
@@ -12,16 +12,48 @@ public class HalftoneDriver
 {
 	public static void main (String [] args) throws Exception
 	{	
-		/* Below is the call to halftone a given image. Note that as it was required to run the 
-		 * application in eclipse in order to be able to catch it in time to profile it, the arguments
-		 * of the image to be processed is directly passed in where:
-		 * first arg = name of file to read in
-		 * second arg = name of file to save out
-		 * third arg = size of dot to draw
-		 */
-		Image image = new Image("person.jpg", "person.png", 10);
+		int gridSize = 0;
 		
-		image.convertToHalftone();
-		image.write();
+		// Expect exactly 3 arguments, otherwise output an exception
+		if(args.length == 3)
+		{
+			/* Handle argument type issues (i.e. empty arguments OR invalid file with no file extension passed as input / output file name
+			*  OR third argument is not an integer)
+			*/
+			for(String arg : args)
+			{
+				if(arg.length() == 0)
+					throw new Exception("One or more of the arguments are empty. Please populate arguments with: " +
+					" input file, output file, grid size");
+			}
+			
+			if(args[0].indexOf('.') == -1 || args[1].indexOf('.') == -1)
+				throw new Exception("The file extention for the file that you are reading in and the file " +
+						"that you wish to output must be included.");
+			
+			try
+			{
+				gridSize = Integer.parseInt(args[2]);
+			}
+			catch(Exception e)
+			{
+				throw new Exception("The 3rd parameter must be an integer value representing the grid size");
+			}
+			
+			/*
+			 * Load in the image, convert the image to grayscale and scale the image down on initialisation.
+			 * Convert the image to halftone and then output the image.
+			 * 
+			 * If the image loaded in is of a type unsupported OR is not an image OR is not an image but has been given an image extension
+			 * like .png or .jpeg, an exception is thrown
+			 */
+			Image image = new Image(args[0], args[1], gridSize);
+			
+			image.convertToHalftone();
+			image.write();
+		}
+		else
+			throw new Exception("Incorrect number of arguments inputted. Arguments should be: " +
+					" input file, output file, grid size");
 	}
 }
